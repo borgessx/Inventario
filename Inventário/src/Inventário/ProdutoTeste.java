ba866a6ea493ec13f1b6c7a36517f7d3428ab320
Aluno <Aluno@10.85.198.15>
2024-12-11 20:50:23 -0300

    JF 7 1 teste commit

--- Inventário/src/Inventário/ProdutoTeste.java
+++ Inventário/src/Inventário/ProdutoTeste.java
@@ -1,5 +1,6 @@
 package Inventário;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ProdutoTeste {
@@ -12,45 +13,48 @@ public class ProdutoTeste {
 		String tempNome;
 		int tempQtd;
 		double tempPreço;
-		int maxSize;
+		int maxSize=-1;
 		
-		System.out.print("\n\nPor favor insira o nome do produto:");
-		tempNome = in.nextLine();
-		System.out.print("Insira a quantidade em estoque deste produto:");
-		tempQtd = in.nextInt();
-		System.out.print("Por favor insira o preço deste produto:");
-		tempPreço = in.nextDouble();
-		System.out.print("Por favor insira o número do item:");
-		tempNúmero = in.nextInt();
-		
-		produto p1 = new produto(tempNúmero, tempNome, tempQtd, tempPreço);
-		in.nextLine();
-		System.out.print("\n\nInsira o nome do produto: ");
-		tempNome = in.nextLine();
-		System.out.print("Insira a quantidade em estoque para este produto: ");
-		tempQtd = in.nextInt();
-		System.out.print("Insira o preço para este produto: ");
-		tempPreço = in.nextDouble();
-		System.out.print("Insira o número do item: ");
-		tempNúmero = in.nextInt();
-		
-		produto p2 = new produto();
-		in.close();
+		do {
+			try {
+				System.out.println("insira o número de produtos que você gostaria de adicionar");
+				System.out.println("Digite 0(zero) se não desejar adicionar produtos");
+				maxSize = in.nextInt();
+				if(maxSize<0)
+					System.out.println("Valor incorreto inserido");
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Tipo de dados incorreto inserido");
+				in.nextLine();
+			}
+			catch(Exception e) {
+					System.out.println(e);
+					in.nextLine();
+			}
+		}while(maxSize<0);
+		if(maxSize ==0) {
+			
+			System.out.println("Não há produtos!");
+		}else {
+			produto[] produtos = new produto[maxSize];
+			for(int i = 0; i<maxSize; i++) {
+				in.nextLine();
 
-		
-		produto p3 = new produto(3,"Tiros Quentes",75, 8.99);
-		produto p4 = new produto(4,"Irmãos de armas", 200, 7.75);
-		produto p5 = new produto(5,"Fantástico", 500, 9.50);
-		produto p6 = new produto(6,"O álbum da festa", 150, 4.00);
-		
-		p6.setativo(false);
-		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-		System.out.println(p4);
-		System.out.println(p5);
-		System.out.println(p6);
+				System.out.print("\n\nInsira o nome do produto: ");
+				tempNome = in.nextLine();
+				System.out.print("Insira a quantidade em estoque para este produto: ");
+				tempQtd = in.nextInt();
+				System.out.print("Insira o preço para este produto: ");
+				tempPreço = in.nextDouble();
+				System.out.print("Insira o número do item: ");
+	
+				tempNúmero = in.nextInt();
+				
+				produtos[i] = new produto(tempNúmero, tempNome, tempQtd, tempPreço);
+			}//endfor
+			
+			for(produto produto: produtos)
+				System.out.println(produto);
+			}
+		}
 	}
-
-}
